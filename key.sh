6c6eb6dc50598c4f574f20b6cd0e094ffd4d54f9
P3TERX <p3terx@outlook.com>
2020-05-07 03:11:24 +0800

    Version 2.7: Optimization log and error prompt

--- key.sh
+++ key.sh
@@ -2,12 +2,18 @@
 #=============================================================
 # https://github.com/P3TERX/SSH_Key_Installer
 # Description: Install SSH keys via GitHub, URL or local files
-# Version: 2.6
+# Version: 2.7
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=============================================================
 
-VERSION=2.6
+VERSION=2.7
+RED_FONT_PREFIX="\033[31m"
+LIGHT_GREEN_FONT_PREFIX="\033[1;32m"
+FONT_COLOR_SUFFIX="\033[0m"
+INFO="[${LIGHT_GREEN_FONT_PREFIX}INFO${FONT_COLOR_SUFFIX}]"
+ERROR="[${RED_FONT_PREFIX}ERROR${FONT_COLOR_SUFFIX}]"
+[ $EUID != 0 ] && SUDO=sudo
 
 USAGE() {
     echo "
@@ -33,16 +39,16 @@ fi
 get_github_key() {
     if [ "${KEY_ID}" == '' ]; then
         read -e -p "Please enter the GitHub account:" KEY_ID
-        [ "${KEY_ID}" == '' ] && echo "Error: Invalid input." && exit 1
+        [ "${KEY_ID}" == '' ] && echo -e "${ERROR} Invalid input." && exit 1
     fi
-    echo "The GitHub account is: ${KEY_ID}"
-    echo "Get key from GitHub..."
+    echo -e "${INFO} The GitHub account is: ${KEY_ID}"
+    echo -e "${INFO} Get key from GitHub..."
     PUB_KEY=$(curl -fsSL https://github.com/${KEY_ID}.keys)
     if [ "${PUB_KEY}" == 'Not Found' ]; then
-        echo "Error: GitHub account not found."
+        echo -e "${ERROR} GitHub account not found."
         exit 1
     elif [ "${PUB_KEY}" == '' ]; then
-        echo "Error: This account ssh key does not exist."
+        echo -e "${ERROR} This account ssh key does not exist."
         exit 1
     fi
 }
@@ -50,77 +56,95 @@ get_github_key() {
 get_url_key() {
     if [ "${KEY_URL}" == '' ]; then
         read -e -p "Please enter the URL:" KEY_URL
-        [ "${KEY_URL}" == '' ] && echo "Error: Invalid input." && exit 1
+        [ "${KEY_URL}" == '' ] && echo -e "${ERROR} Invalid input." && exit 1
     fi
-    echo "Get key from URL..."
+    echo -e "${INFO} Get key from URL..."
     PUB_KEY=$(curl -fsSL ${KEY_URL})
 }
 
 get_loacl_key() {
     if [ "${KEY_PATH}" == '' ]; then
         read -e -p "Please enter the path:" KEY_PATH
-        [ "${KEY_PATH}" == '' ] && echo "Error: Invalid input." && exit 1
+        [ "${KEY_PATH}" == '' ] && echo -e "${ERROR} Invalid input." && exit 1
     fi
-    echo "Get key from $(${KEY_PATH})..."
+    echo -e "${INFO} Get key from $(${KEY_PATH})..."
     PUB_KEY=$(cat ${KEY_PATH})
 }
 
 install_key() {
-    [ "${PUB_KEY}" == '' ] && echo "Error: ssh key does not exist." && exit 1
+    [ "${PUB_KEY}" == '' ] && echo "${ERROR} ssh key does not exist." && exit 1
     if [ ! -f "${HOME}/.ssh/authorized_keys" ]; then
-        echo "${HOME}/.ssh/authorized_keys is missing..."
-        echo "Creating ${HOME}/.ssh/authorized_keys..."
+        echo -e "${INFO} '${HOME}/.ssh/authorized_keys' is missing..."
+        echo -e "${INFO} Creating ${HOME}/.ssh/authorized_keys..."
         mkdir -p ${HOME}/.ssh/
         touch ${HOME}/.ssh/authorized_keys
         if [ ! -f "${HOME}/.ssh/authorized_keys" ]; then
-            echo "Failed to create SSH key file."
+            echo -e "${ERROR} Failed to create SSH key file."
         else
-            echo "Key file created, proceeding..."
+            echo -e "${INFO} Key file created, proceeding..."
         fi
     fi
     if [ "${OVERWRITE}" == 1 ]; then
-        echo "Overwriting SSH key..."
+        echo -e "${INFO} Overwriting SSH key..."
         echo -e "${PUB_KEY}\n" >${HOME}/.ssh/authorized_keys
     else
-        echo "Adding SSH key..."
+        echo -e "${INFO} Adding SSH key..."
         echo -e "\n${PUB_KEY}\n" >>${HOME}/.ssh/authorized_keys
     fi
     chmod 700 ${HOME}/.ssh/
     chmod 600 ${HOME}/.ssh/authorized_keys
     [[ $(grep "${PUB_KEY}" "${HOME}/.ssh/authorized_keys") ]] &&
-        echo "SSH Key installed successfully!"
+        echo -e "${INFO} SSH Key installed successfully!" || {
+        echo -e "${ERROR} SSH key installation failed!"
+        exit 1
+    }
 }
 
 change_port() {
-    echo "Changing SSH port to ${SSH_PORT} ..."
+    echo -e "${INFO} Changing SSH port to ${SSH_PORT} ..."
     if [ $(uname -o) == Android ]; then
         [[ -z $(grep "Port " "$PREFIX/etc/ssh/sshd_config") ]] &&
-            echo "Port ${SSH_PORT}" >>$PREFIX/etc/ssh/sshd_config ||
+            echo -e "${INFO} Port ${SSH_PORT}" >>$PREFIX/etc/ssh/sshd_config ||
             sed -i "s@.*\(Port \).*@\1${SSH_PORT}@" $PREFIX/etc/ssh/sshd_config
         [[ $(grep "Port " "$PREFIX/etc/ssh/sshd_config") ]] && {
-            echo "SSH port changed successfully !"
+            echo -e "${INFO} SSH port changed successfully!"
             RESTART_SSHD=2
-        } || RESTART_SSHD=0
-
+        } || {
+            RESTART_SSHD=0
+            echo -e "${ERROR} SSH port change failed!"
+            exit 1
+        }
     else
         $SUDO sed -i "s@.*\(Port \).*@\1${SSH_PORT}@" /etc/ssh/sshd_config && {
-            echo "SSH port changed successfully !"
+            echo -e "${INFO} SSH port changed successfully!"
             RESTART_SSHD=1
-        } || RESTART_SSHD=0
+        } || {
+            RESTART_SSHD=0
+            echo -e "${ERROR} SSH port change failed!"
+            exit 1
+        }
     fi
 }
 
 disable_password() {
-    echo "Disabled password login in SSH."
     if [ $(uname -o) == Android ]; then
-        sed -i "s@.*\(PasswordAuthentication \).*@\1no@" $PREFIX/etc/ssh/sshd_config &&
-            RESTART_SSHD=2 ||
+        sed -i "s@.*\(PasswordAuthentication \).*@\1no@" $PREFIX/etc/ssh/sshd_config && {
+            RESTART_SSHD=2
+            echo -e "${INFO} Disabled password login in SSH."
+        } || {
             RESTART_SSHD=0
+            echo -e "${ERROR} Disable password login failed!"
+            exit 1
+        }
     else
-        [ $EUID != 0 ] && SUDO=sudo
-        $SUDO sed -i "s@.*\(PasswordAuthentication \).*@\1no@" /etc/ssh/sshd_config &&
-            RESTART_SSHD=1 ||
+        $SUDO sed -i "s@.*\(PasswordAuthentication \).*@\1no@" /etc/ssh/sshd_config && {
+            RESTART_SSHD=1
+            echo -e "${INFO} Disabled password login in SSH."
+        } || {
             RESTART_SSHD=0
+            echo -e "${ERROR} Disable password login failed!"
+            exit 1
+        }
     fi
 }
 
@@ -167,8 +191,8 @@ while getopts "og:u:f:p:d" OPT; do
 done
 
 if [ "$RESTART_SSHD" = 1 ]; then
-    echo "Restarting sshd..."
-    $SUDO systemctl restart sshd && echo "Done."
+    echo -e "${INFO} Restarting sshd..."
+    $SUDO systemctl restart sshd && echo -e "${INFO} Done."
 elif [ "$RESTART_SSHD" = 2 ]; then
-    echo "Restart sshd or Termux App to take effect."
+    echo -e "${INFO} Restart sshd or Termux App to take effect."
 fi
